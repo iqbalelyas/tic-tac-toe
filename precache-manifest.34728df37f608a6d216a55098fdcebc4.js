c9133cc5340885b54b242c1eac3a55c33e239aeb
iqbalelyas <iqbal35mm@gmail.com>
2019-11-18 10:32:24 +0700

    Updates

---
+++ precache-manifest.34728df37f608a6d216a55098fdcebc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910f32a1920b757aa9a1991f37b025bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "457be42e257f6b117abb",
+    "url": "/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "b9f734d9182e84131b20",
+    "url": "/static/js/2.87ab57ce.chunk.js"
+  },
+  {
+    "revision": "457be42e257f6b117abb",
+    "url": "/static/js/main.54785ac0.chunk.js"
+  },
+  {
+    "revision": "7190e0bf4153bb493e36",
+    "url": "/static/js/runtime-main.ddf7af3e.js"
+  }
+]);
